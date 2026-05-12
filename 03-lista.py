f7b1f57456495fae84fb475e4e5929e7b157cdbc
Miguel Pérez <miguel2005perez@gmail.com>
2026-05-12 17:01:02 -0400

    subiendo archivo de lista

---
+++ 03-lista.py
@@ -0,0 +1,15 @@
+#lista
+
+#primera forma: declarar una lista utilizando corchetes []
+lista1= ["Miguel", 20, 1.75, True]
+
+#segunda forma: utilizando la función list()
+lista2 = list(["Ana", 25, 1.65, False])
+
+
+print(lista1)
+print(lista2)
+
+#metodos para la listas
+#01. Count()
+print(lista1.count(1))
